c5d8af8d7b91eb8e8a213f7dff024a11bad9f270
Dany <danielvillasantioficial@gmail.com>
2022-08-17 08:06:12 -0400

    ahora algunos datos no son obligatorios

--- apps/core/models.py
+++ apps/core/models.py
@@ -11,7 +11,7 @@ class DifficultyTask(models.Model):
 class Task(models.Model):
 	name = models.CharField(max_length=50)
 	difficulty = models.ForeignKey(DifficultyTask, on_delete=models.PROTECT)
-	summary = models.TextField()
+	summary = models.TextField(blank=True, null=True)
 	id_user = models.CharField(max_length=100)
 	created = models.DateTimeField(auto_now_add=True)
 	modified = models.DateTimeField(auto_now=True)
