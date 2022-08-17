939ccdda8347661f372c6e62000ee827742593a7
Dany <danielvillasantioficial@gmail.com>
2022-08-17 08:05:46 -0400

    se crearon los modelos

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class DifficultyTask(models.Model):
+	name = models.CharField(max_length=15)
+
+	def __str__(self) -> str:
+		return f'{self.name}'
+
+class Task(models.Model):
+	name = models.CharField(max_length=50)
+	difficulty = models.ForeignKey(DifficultyTask, on_delete=models.PROTECT)
+	summary = models.TextField()
+	id_user = models.CharField(max_length=100)
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
+
+	def __str__(self) -> str:
+		return f'{self.name}'
