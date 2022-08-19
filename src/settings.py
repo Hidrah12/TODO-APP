cbf356721921b045e47f49344e32a8adaeb76cd6
Dany <danielvillasantioficial@gmail.com>
2022-08-19 12:09:14 -0400

    se cambio la configuracion

--- src/settings.py
+++ src/settings.py
@@ -119,5 +119,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-if config('DJANGO_PRODUCTION', default=False, cast=bool):
+if config('PRODUCTION', default=False, cast=bool):
     from .production import *
