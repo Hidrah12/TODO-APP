111dc968dd72319fb445975f7cad70b5502a64fa
Dany <danielvillasantioficial@gmail.com>
2022-08-19 09:20:59 -0400

    refac: se mejoro el codigo javascript

--- static/js/index.js
+++ static/js/index.js
@@ -5,8 +5,9 @@ description.placeholder = 'Description'
 
 let names = document.getElementsByClassName('name')
 let contador = document.getElementById('contador').innerHTML = names.length
-let or = document.getElementsByClassName('name-important')
-let e = document.getElementById('total-important').innerHTML = or.length
+let important_tasks = document.getElementsByClassName('name-important')
+document.getElementById('total-important').innerHTML = important_tasks.length
+
 function addNewTask() {
 	let menuTask = document.getElementById('menu-task')
 	if (menuTask.classList.toString().includes('invisible')) {
@@ -32,10 +33,10 @@ function importants() {
 	})
 	let importants = document.getElementById('importants')
 	importants.className = 'option p-6 bsm:p-4 border-b-2 border-purple-700'
-	let task_important = document.getElementById('task-list-important').style.display = 'block'
+	document.getElementById('task-list-important').style.display = 'block'
 }
 function tasks() {
-	let task_important = document.getElementById('task-list-important').style.display = 'none'
+	document.getElementById('task-list-important').style.display = 'none'
 	document.getElementById('task-list').style.display = 'block'
 	let options = document.getElementsByClassName('option')
 	Array.from(options).forEach(element => {
